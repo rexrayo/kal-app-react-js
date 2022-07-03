27edb33682e2b17dafa63784fb105f661368fcd2
kaller <rexrayo15@gmail.com>
2022-07-03 19:28:40 -0400

    Test PWA

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
- 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -14,3 +15,5 @@ root.render(
         </BrowserRouter>
     </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
